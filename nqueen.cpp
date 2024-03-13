e71c9270b8e080567142600899995301470ba4f7
SHOB RAJ GOWDA K <94729553+ShobRaj24@users.noreply.github.com>
2024-03-13 17:54:53 +0530

    nqueen.cpp

--- nqueen.cpp
+++ nqueen.cpp
@@ -1,96 +1,100 @@
-
-
-#include <bits/stdc++.h>
-#define N 8
+#include <iostream>
 using namespace std;
 
-
-void printSolution(int board[N][N])
+class NQueen
 {
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < N; j++)
-		if(board[i][j])
-			cout << "Q ";
-		else cout<<". ";
-		cout<<"\n";
-	}
+public:
+    void printSolution(int board[][8], int N);
+    void solveNQUtil(int board[][8], int col, int N);
+    bool isSafe(int board[][8], int row, int col, int N);
+    void solveNQ(int N);
+};
+
+void NQueen::printSolution(int board[][8], int N)
+{
+    static int solutionCount = 0;
+    cout << "Solution " << ++solutionCount << ":\n";
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            if (board[i][j])
+                cout << "P ";
+            else
+                cout << ". ";
+        }
+        cout << "\n";
+    }
+    cout << "\n";
 }
 
-
-bool isSafe(int board[N][N], int row, int col)
+bool NQueen::isSafe(int board[][8], int row, int col, int N)
 {
-	int i, j;
+    int i, j;
 
-	// Check this row on left side
-	for (i = 0; i < col; i++)
-		if (board[row][i])
-			return false;
+    for (i = 0; i < col; i++)
+        if (board[row][i])
+            return false;
 
-	 // Check upper diagonal on left side
-	for (i = row, j = col; i >= 0 && j >= 0; i--, j--)
-		if (board[i][j])
-			return false;
+    for (i = row, j = col; i >= 0 && j >= 0; i--, j--)
+        if (board[i][j])
+            return false;
 
-	
-    // Check lower diagonal on left side
-	for (i = row, j = col; j >= 0 && i < N; i++, j--)
-		if (board[i][j])
-			return false;
+    for (i = row, j = col; j >= 0 && i < N; i++, j--)
+        if (board[i][j])
+            return false;
 
-	return true;
+    return true;
 }
 
-
-bool solveNQUtil(int board[N][N], int col)
+void NQueen::solveNQUtil(int board[][8], int col, int N)
 {
-	
-	if (col >= N)
-		return true;
+    if (col >= N)
+    {
+        printSolution(board, N);
+        return;
+    }
 
-	
-	for (int i = 0; i < N; i++) {
-		
-		
-		if (isSafe(board, i, col)) {
-			
-			
-			board[i][col] = 1;
-
-			
-			if (solveNQUtil(board, col + 1))
-				return true;
-
-			
-			board[i][col] = 0; 
-		}
-	}
-
-	
-	return false;
+    for (int i = 0; i < N; i++)
+    {
+        if (isSafe(board, i, col, N))
+        {
+            board[i][col] = 1;
+            solveNQUtil(board, col + 1, N);
+            board[i][col] = 0; // Backtrack
+        }
+    }
 }
 
-
-bool solveNQ()
+void NQueen::solveNQ(int N)
 {
-	int board[N][N] = { { 0, 0, 0, 0 },
-						{ 0, 0, 0, 0 },
-						{ 0, 0, 0, 0 },
-						{ 0, 0, 0, 0 } };
+    int board[8][8]; // Assuming the board size is fixed at 8x8
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            board[i][j] = 0;
+        }
+    }
 
-	if (solveNQUtil(board, 0) == false) {
-		cout << "Solution does not exist";
-		return false;
-	}
-
-	printSolution(board);
-	return true;
+    solveNQUtil(board, 0, N);
 }
-
 
 int main()
 {
-	solveNQ();
-	return 0;
+    int N;
+    cout << "Enter the number of queens: ";
+    cin >> N;
+    if(N==2 || N==3){
+        cout<<"No solution for "<<N<<"\n";
+    }
+    NQueen nq;
+
+    clock_t start = clock();
+    nq.solveNQ(N);
+    clock_t end = clock();
+
+    double execution_time = double(end - start) / double(CLOCKS_PER_SEC);
+    cout << "The time taken by NQueen is " << fixed << execution_time << " seconds";
+    return 0;
 }
-
-
